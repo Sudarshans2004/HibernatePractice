76880fabb6f186c2f2ebca3166823c4528c08d24
sudarshan shinde <sudarshanshinde033@gmail.com>
2026-06-23 18:05:25 +0530

    hibernate

---
+++ src/main/java/mapping/Answer.java
@@ -0,0 +1,51 @@
+package mapping;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+
+@Entity
+public class Answer {
+    public Answer(int answerId, String answer) {
+        this.answerId = answerId;
+        this.answer = answer;
+    }
+
+    public Question getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(Question question) {
+        this.question = question;
+    }
+
+    @OneToOne
+    private Question question;
+
+    public Answer() {
+    }
+
+    public int getAnswerId() {
+        return answerId;
+    }
+
+    public void setAnswerId(int answerId) {
+        this.answerId = answerId;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+    @Id
+    @Column(name = "answer")
+     private int answerId;
+    @Column(name = "answer")
+     private String answer;
+
+}
