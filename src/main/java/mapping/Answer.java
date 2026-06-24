473d1afa45f6b59ad2f2ccfb0c03a85d70b9a3eb
sudarshan shinde <sudarshanshinde033@gmail.com>
2026-06-24 21:20:16 +0530

    hibernate practice

--- src/main/java/mapping/Answer.java
+++ src/main/java/mapping/Answer.java
@@ -1,9 +1,6 @@
 package mapping;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToOne;
+import jakarta.persistence.*;
 
 @Entity
 public class Answer {
@@ -20,7 +17,7 @@ public class Answer {
         this.question = question;
     }
 
-    @OneToOne
+    @ManyToOne
     private Question question;
 
     public Answer() {
@@ -43,7 +40,7 @@ public class Answer {
     }
 
     @Id
-    @Column(name = "answer")
+    @Column(name = "answer_id")
      private int answerId;
     @Column(name = "answer")
      private String answer;
