42507f17ac7a328e8aa9ad66a55f4bff47341f47
sudarshan shinde <sudarshanshinde033@gmail.com>
2026-08-04 15:44:20 +0530

    hibernate practice

---
+++ src/main/java/hibernateplayground/entity/Student.java
@@ -0,0 +1,60 @@
+package hibernateplayground.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Student {
+
+    @Column(name = "Name")
+    private String name;
+    @Column(name = "City")
+    private String city;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(name = "age")
+    private int age;
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", city='" + city + '\'' +
+                ", id=" + id +
+                ", age=" + age +
+                '}';
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+}
