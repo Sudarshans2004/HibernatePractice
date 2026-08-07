2c5920507cd49cb329e6c46634986417f7d62a29
sudarshan shinde <sudarshanshinde033@gmail.com>
2026-08-07 19:26:24 +0530

    hibernate practice

---
+++ src/main/java/criteria/Sttudent.java
@@ -0,0 +1,21 @@
+package criteria;
+
+import java.io.Serializable;
+
+public class Sttudent implements Serializable {
+
+    private static final long serialVersionUID = 123L;
+
+    int id;
+    String name;
+
+    public Sttudent(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{id=" + id + ", name='" + name + "'}";
+    }
+}
